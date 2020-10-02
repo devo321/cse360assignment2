5db7714bf0db421d9c3e75adababa087195e8972
devo321 <devo321@gmail.com>
2020-10-01 20:52:10 -0700

    initial commit

---
+++ cse360assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,47 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+
+
+  /**
+   * Returns total
+   * @return    total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+
+  /**
+   * Adds param to total
+   * @param value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * Subtracts param from total 
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+  
+  /**
+   * Converts operations to string
+   * @return string The string of all the operations done to total 
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * Clears string
+   */
+  public void clear() {
+  }
+}
